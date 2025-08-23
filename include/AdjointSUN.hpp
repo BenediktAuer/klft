09d9e2f04e15be369a7717835ec943f36f004dd9
BenediktAuer <benedikt_auer01@web.de>
2025-08-23 13:33:00 +0200

    Fix formatting and logic errors in AdjointSUN.hpp

--- include/AdjointSUN.hpp
+++ include/AdjointSUN.hpp
@@ -32,8 +32,7 @@ namespace klft {
 
 template <size_t Nc>
 KOKKOS_FORCEINLINE_FUNCTION void print_SUNAdj(
-    const SUNAdj<Nc>& a,
-    const std::string& name = "SUNAdj:") {
+    const SUNAdj<Nc>& a, const std::string& name = "SUNAdj:") {
   printf("%s\n", name.c_str());
   for (size_t i = 0; i < Nc * Nc - 1; ++i) {
     printf("    [%zu] = (% .20f)\n", i, a[i]);
@@ -172,7 +171,7 @@ SUNAdj<3> traceT(const SUN<3>& a) {
   c[6] = 0.5 * (a[2][1].real() - a[1][2].real());
   c[7] = 0.5 *
          ((-a[0][0].imag() - a[1][1].imag() + 2.0 * a[2][2].imag()) * SQRT3INV);
-  return -2 * c;
+  return -1 * c;
 }
 
 // exponential of an adjoint matrix
@@ -215,15 +214,18 @@ constexpr size_t n_iter_expoSU3 = []() {
   return n_iter;
 }();
 // we also need to setup the expansion coefficients
-constexpr Kokkos::Array<real_t, n_iter_expoSU3 + 1> coeffs_expoSU3 = []() {
-  Kokkos::Array<real_t, n_iter_expoSU3 + 1> coeffs{};
-  coeffs[0] = 1.0;
+// static such that cuda dosnt complain when compiling with gcc
+static KOKKOS_FORCEINLINE_FUNCTION constexpr Kokkos::Array<real_t,
+                                                           n_iter_expoSU3 + 1>
+    coeffs_expoSU3 = []() {
+      Kokkos::Array<real_t, n_iter_expoSU3 + 1> coeffs{};
+      coeffs[0] = 1.0;
 
-  for (int i = 0; i < n_iter_expoSU3; i++) {
-    coeffs[i + 1] = coeffs[i] / static_cast<real_t>(i + 1);
-  }
-  return coeffs;
-}();
+      for (int i = 0; i < n_iter_expoSU3; i++) {
+        coeffs[i + 1] = coeffs[i] / static_cast<real_t>(i + 1);
+      }
+      return coeffs;
+    }();
 
 // a function to generate SU(3) matrix from adjoint
 constexpr KOKKOS_FORCEINLINE_FUNCTION SUN<3> get_SU3_from_adj(
@@ -238,12 +240,12 @@ constexpr KOKKOS_FORCEINLINE_FUNCTION SUN<3> get_SU3_from_adj(
   c[2][0] = 0.5 * complex_t(-a[4], a[3]);
   c[2][1] = 0.5 * complex_t(-a[6], a[5]);
   c[2][2] = complex_t(0.0, -SQRT3INV * a[7]);
-  return 2 * c;
+  return c;
 }
 
 // we also need the determinant of the SU(3) matrix
 // returns i det(a)
-constexpr KOKKOS_FORCEINLINE_FUNCTION real_t imag_det_SU3(const SUNAdj<3>& a) {
+KOKKOS_FORCEINLINE_FUNCTION real_t imag_det_SU3(const SUNAdj<3>& a) {
   real_t d = -2.0 * SQRT3INV * a[7] * (a[7] * a[7] / 3.0 - a[2] * a[2]) -
              2.0 * (a[1] * a[3] * a[6] - a[0] * a[3] * a[5] -
                     a[1] * a[4] * a[5] - a[0] * a[4] * a[6]);
