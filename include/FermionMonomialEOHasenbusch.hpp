54af21d8d8c721cde1059594e388b20c1f2bfd7d
Benedikt Auer <benedikt_auer01@web.de>
2026-01-17 13:34:31 +0100

    [Add] Inplace operations

--- include/FermionMonomialEOHasenbusch.hpp
+++ include/FermionMonomialEOHasenbusch.hpp
@@ -65,6 +65,18 @@ class FermionMonomialEOHasenbusch
   using DiracOpNonShift = DiracOPNonShift;
   const diracParams params;
 
+  FermionField x;
+
+  FermionField x0;
+  // auxillary fields
+  FermionField xk;
+  FermionField rk;
+  FermionField apk;
+  FermionField temp_D;
+  typename DeviceScalarFieldType<rank>::type norm_per_site;
+  typename DeviceFieldType<rank>::type dot_product_per_site;
+  FermionField pk;
+
   const real_t tol;
   RNGType rng;
   FermionMonomialEOHasenbusch(FermionField& _phi,
@@ -78,6 +90,19 @@ class FermionMonomialEOHasenbusch
 
         rng(RNG_),
         tol(tol_) {
+    this->x = FermionField(this->phi.dimensions, complex_t(0.0, 0.0));
+
+    this->x0 = FermionField(this->phi.dimensions, complex_t(0.0, 0.0));
+    // Auxillary
+    this->xk = FermionField(phi.dimensions, complex_t(0.0, 0.0));
+    this->rk = FermionField(phi.dimensions, complex_t(0.0, 0.0));
+    this->apk = FermionField(phi.dimensions, complex_t(0.0, 0.0));
+    this->temp_D = FermionField(phi.dimensions, complex_t(0.0, 0.0));
+    this->pk = FermionField(phi.dimensions, complex_t(0.0, 0.0));
+    this->norm_per_site =
+        typename DeviceScalarFieldType<rank>::type(phi.dimensions, 0.0);
+    this->dot_product_per_site = typename DeviceFieldType<rank>::type(
+        phi.dimensions, complex_t(0.0, 0.0));
     Monomial<DGaugeFieldType, DAdjFieldType>::monomial_type =
         KLFT_MONOMIAL_FERMION;
     printf("Created Fermion HB Monomial EO\n");
@@ -86,19 +111,18 @@ class FermionMonomialEOHasenbusch
   void heatbath(HamiltonianField<DGaugeFieldType, DAdjFieldType> h) override {
     Kokkos::Profiling::pushRegion("FermionHeatbathEO");
     auto dims = phi.dimensions;
-    FermionField x(dims, complex_t(0.0, 0.0));
-    FermionField x0(dims, complex_t(0.0, 0.0));
-    FermionField temp(dims, complex_t(0.0, 0.0));
     DiracOpNonShift D_n(h.gauge_field, this->params);
     DiracOpShifted D_s(h.gauge_field, this->params);
     FermionField R(dims, rng, 0, SQRT2INV);
-    D_n.template apply<Tags::TagG5Se>(R, this->phi, temp);
-    Solver solver(temp, x, D_s);
+    D_n.template apply<Tags::TagG5Se>(R, this->temp_D, this->phi);
+    Solver solver(this->phi, this->x, D_s, this->xk, this->rk, this->apk,
+                  this->temp_D, this->pk, this->norm_per_site,
+                  this->dot_product_per_site);
     solver.template solve<Tags::TagDdaggerD>(
-        x0,
+        this->x0,
         this->tol);  // chi = S_e^-1 S_e^-1 R
 
-    D_s.template apply<Tags::TagG5Se>(solver.x, x0, this->phi);
+    D_s.template apply<Tags::TagG5Se>(solver.x, this->x0, this->phi);
 
     Monomial<DGaugeFieldType, DAdjFieldType>::H_old =
         spinor_norm_sq<rank, Nc, RepDim>(R);
@@ -110,20 +134,19 @@ class FermionMonomialEOHasenbusch
     Kokkos::Profiling::pushRegion("FermionAcceptEO");
     auto dims = phi.dimensions;
 
-    FermionField x(dims, complex_t(0.0, 0.0));
-    FermionField x0(dims, complex_t(0.0, 0.0));
     FermionField y(dims, complex_t(0.0, 0.0));
-    FermionField temp(dims, complex_t(0.0, 0.0));
 
     DiracOpNonShift D_n(h.gauge_field, this->params);
     DiracOpShifted D_s(h.gauge_field, this->params);
-    D_s.template apply<Tags::TagG5Se>(this->phi, temp, y);
-    _Solver<DiracOpNonShift> solver(y, x, D_n);
+    D_s.template apply<Tags::TagG5Se>(this->phi, this->temp_D, y);
+    _Solver<DiracOpNonShift> solver(
+        y, this->x, D_n, this->xk, this->rk, this->apk, this->temp_D, this->pk,
+        this->norm_per_site, this->dot_product_per_site);
     if (KLFT_VERBOSITY > 4) {
       printf("Solving inside Fermion Monomial accept:");
     }
 
-    solver.template solve<Tags::TagDdaggerD>(x0, this->tol);
+    solver.template solve<Tags::TagDdaggerD>(this->x0, this->tol);
 
     Monomial<DGaugeFieldType, DAdjFieldType>::H_new =
         spinor_dot_product<rank, Nc, RepDim>(y, solver.x).real();
