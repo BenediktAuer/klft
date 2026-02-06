1d9a70a0c527cccca04f60af75bf6d57c26e0512
Benedikt Auer <benedikt_auer01@web.de>
2026-02-06 16:30:34 +0100

    [fix] .

--- include/Solver.hpp
+++ include/Solver.hpp
@@ -513,17 +513,17 @@ class CGMultiP : public Solver<CGMultiP<DiracOpT, precision>, DiracOpT> {
     this->rk = SpinorFieldType(this->dims, complex_t(0.0, 0.0));
     this->temp_D_full_complexity =
         SpinorFieldType(this->dims, complex_t(0.0, 0.0));
-    this->apk = SloppySpinorField(
-        this->dims, Kokkos::complex<typename precision::value_type>(0.0, 0.0));
+    this->apk = SloppySpinorField(this->dims,
+                                  Kokkos::complex<complexsingle_t>(0.0, 0.0));
     this->temp_D = SloppySpinorField(
-        this->dims, Kokkos::complex<typename precision::value_type>(0.0, 0.0));
+        this->dims, Kokkos::complex<complexsingle_t>(0.0, 0.0));
     this->x_sloppy = SloppySpinorField(
-        this->dims, Kokkos::complex<typename precision::value_type>(0.0, 0.0));
+        this->dims, Kokkos::complex<complexsingle_t>(0.0, 0.0));
     this->r_sloppy = SloppySpinorField(
-        this->dims, Kokkos::complex<typename precision::value_type>(0.0, 0.0));
+        this->dims, Kokkos::complex<complexsingle_t>(0.0, 0.0));
 
-    this->pk = SloppySpinorField(
-        this->dims, Kokkos::complex<typename precision::value_type>(0.0, 0.0));
+    this->pk = SloppySpinorField(this->dims,
+                                 Kokkos::complex<complexsingle_t>(0.0, 0.0));
     this->norm_per_site =
         typename DeviceScalarFieldType<rank>::type(this->dims, 0.0);
     this->dot_product_per_site =
