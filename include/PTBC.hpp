e5aafb50eb644e3182b75353a2a563c804fe586a
Benedikt Auer <benedikt_auer01@web.de>
2026-03-06 21:48:46 +0100

    [Fix] EO prec for PTBC Pion cor measurement

--- include/PTBC.hpp
+++ include/PTBC.hpp
@@ -236,19 +236,20 @@ class PTBC {  // do I need the AdjFieldType here?
           printf("Measuring   Fermionic observables at step %zu\n", step);
         }
         measureFermionObservablesPTBC<
-            std::mt19937, CGSolver,
-            WilsonDiracOperator<
-                DeviceSpinorFieldType<HMCType::rank, HMCType::Nc, 4, complex_t>,
-                DGaugeFieldType>>(hmc.hamiltonian_field.gauge_field,
-                                  fermionObsParam, step, 0, hmc.mt, true);
+            std::mt19937,
+            DeviceSpinorFieldType<HMCType::rank, HMCType::Nc, 4, complex_t>,
+            DeviceGaugeFieldType<Nd, Nc, complex_t, GaugeFieldKind::Standard>,
+            CGSolver, EOWilsonDiracOperator>(hmc.hamiltonian_field.gauge_field,
+                                             fermionObsParam, step, 0, hmc.mt,
+                                             true);
       } else {
         measureFermionObservablesPTBC<
-            std::mt19937, CGSolver,
-            WilsonDiracOperator<
-                DeviceSpinorFieldType<HMCType::rank, HMCType::Nc, 4, complex_t>,
-                DGaugeFieldType>>(hmc.hamiltonian_field.gauge_field,
-                                  fermionObsParam, step, compute_rank, hmc.mt,
-                                  false);
+            std::mt19937,
+            DeviceSpinorFieldType<HMCType::rank, HMCType::Nc, 4, complex_t>,
+            DeviceGaugeFieldType<Nd, Nc, complex_t, GaugeFieldKind::Standard>,
+            CGSolver, EOWilsonDiracOperator>(hmc.hamiltonian_field.gauge_field,
+                                             fermionObsParam, step,
+                                             compute_rank, hmc.mt, false);
       }
     } else {
       MPI_Reduce(&dummy_rank, &compute_rank, 1, MPI_INT, MPI_SUM, 0,
