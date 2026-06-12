c12b6c719fc777cf5cd654938bcbdd7adb1f2148
Benedikt Auer <benedikt_auer01@web.de>
2026-06-12 22:18:34 +0200

    [Upate, WiP] rect staple

--- include/GaugeField.hpp
+++ include/GaugeField.hpp
@@ -407,153 +407,96 @@ struct deviceGaugeField {
     }  // loop over nu
     return temp;
   }
-
+  // This function calculates the staple and the rect wilson looops for a given
+  // direction mu and site.
   template <typename indexType>
   KOKKOS_FORCEINLINE_FUNCTION SUN<Nc, precision_t> staple_rect(
       const Kokkos::Array<indexType, 4> site,
       const index_t mu) const {
     // this only works if Nd == 4
     assert(Nd == 4);
-    Kokkos::Array<indexType, 4> temp_site1 = site;
-    Kokkos::Array<indexType, 4> temp_site2 = site;
-    Kokkos::Array<indexType, 4> temp_site3 = site;
-    Kokkos::Array<indexType, 4> temp_site4 = site;
 
-    SUN<Nc, precision_t> temp = zeroSUN<Nc, precision_t>();
+    SUN<Nc, precision_t> rect = zeroSUN<Nc, precision_t>();
+    SUN<Nc, precision_t> staple = zeroSUN<Nc, precision_t>();
 
     // Helper lambda for modular arithmetic
     auto mod = [&](index_t s, index_t dir) {
       return (s + this->dimensions[dir]) % this->dimensions[dir];
     };
 
-#pragma unroll
+    // #pragma unroll
     for (index_t nu = 0; nu < Nd; ++nu) {  // loop over nu
       // do nothing for mu = nu
       if (nu == mu)
         continue;
-      // first do the 1x2 rectangles
-      temp_site1[mu] = (temp_site1[mu] + 1) % this->dimensions[mu];
-      temp_site2[nu] = (temp_site2[nu] + 1) % this->dimensions[nu];
-      temp_site2[mu] = (temp_site2[mu] + 1) % this->dimensions[mu];
-      temp_site3[nu] = (temp_site3[nu] + 2) % this->dimensions[nu];
-      temp_site4[nu] = (temp_site4[nu] + 1) % this->dimensions[nu];
-      temp += this->operator()(temp_site1, nu) *
-              this->operator()(temp_site2, nu) *
-              conj(this->operator()(temp_site3, mu)) *
-              conj(this->operator()(temp_site4, nu)) *
-              conj(this->operator()(site, nu));
-      temp_site1 = site;
-      temp_site2 = site;
-      temp_site3 = site;
-      temp_site4 = site;
-      //
-      temp_site1[mu] = (temp_site1[mu] + 1) % this->dimensions[mu];
-      temp_site2[mu] = (temp_site2[mu] + 2) % this->dimensions[mu];
-      temp_site2[nu] = mod(temp_site2[nu] - 1, nu);
-      temp_site3[mu] = (temp_site3[mu] + 1) % this->dimensions[mu];
-      temp_site3[nu] = mod(temp_site3[nu] - 1, nu);
-      temp_site4[nu] = mod(temp_site4[nu] - 1, nu);
-      temp += this->operator()(temp_site1, mu) *
-              conj(this->operator()(temp_site2, nu)) *
-              conj(this->operator()(temp_site3, mu)) *
-              conj(this->operator()(temp_site4, mu)) *
-              this->operator()(temp_site4, nu);
-      temp_site1 = site;
-      temp_site2 = site;
-      temp_site3 = site;
-      temp_site4 = site;
-      //
-      temp_site1[mu] = (temp_site1[mu] + 1) % this->dimensions[mu];
-      temp_site1[nu] = mod(temp_site1[nu] - 1, nu);
-      temp_site2[nu] = mod(temp_site2[nu] - 1, nu);
-      temp_site3[mu] = mod(temp_site3[mu] - 1, mu);
-      temp_site3[nu] = mod(temp_site3[nu] - 1, nu);
-      temp_site4[mu] = mod(temp_site4[mu] - 1, mu);
-      temp += conj(this->operator()(temp_site1, nu)) *
-              conj(this->operator()(temp_site2, mu)) *
-              conj(this->operator()(temp_site3, mu)) *
-              this->operator()(temp_site3, nu) *
-              this->operator()(temp_site4, mu);
-      temp_site1 = site;
-      temp_site2 = site;
-      temp_site3 = site;
-      temp_site4 = site;
-      //
-      // now do the 2x1 rectangles
-      /*
-          "Forward" 2x1 (+ν)
+      // U_nu(x-nu) and U_mu(x-nu)
+      auto x_pmu = shift_index_plus(site, mu, 1, dimensions);
+      auto x_mmu = shift_index_minus(site, mu, 1, dimensions);
 
-            x+ν <--U†(x+ν,μ)-- x+μ+ν <--U†(x+μ+ν,μ)-- x+2μ+ν
-            |                                          ^
-            U†(x,ν)                                 U(x+2μ,ν)
-            v                                          |
-            x ==U(x,μ)==>     x+μ  ---U(x+μ,μ)--->    x+2μ
-      */
-      //       temp_site1[mu] = (temp_site1[mu] + 1) % this->dimensions[mu];
-      //       temp_site2[mu] = (temp_site2[mu] + 2) % this->dimensions[mu];
-      //       temp_site3[mu] = (temp_site3[mu] + 1) % this->dimensions[mu];
-      //       temp_site3[nu] = (temp_site3[nu] + 1) % this->dimensions[nu];
-      //       temp_site4[nu] = (temp_site4[nu] + 1) % this->dimensions[nu];
-      //       temp += this->operator()(temp_site1, mu) *
-      //               this->operator()(temp_site2, nu) *
-      //               conj(this->operator()(temp_site3, mu)) *
-      //               conj(this->operator()(temp_site4, mu)) *
-      //               conj(this->operator()(site, nu));
-      //       temp_site1 = site;
-      //       temp_site2 = site;
-      //       temp_site3 = site;
-      //       temp_site4 = site;
-      //
-      //       /*
-      //        U(x,μ): ==>
-      //         <--  <--
-      //        |        ^
-      //        v        |
-      //         -->  ==>
-      //       */
-      //       temp_site1[mu] = (temp_site1[mu] + 1) % this->dimensions[mu];
-      //       temp_site2[nu] = (temp_site2[nu] + 1) % this->dimensions[nu];
-      //       temp_site3[nu] = (temp_site3[nu] + 1) % this->dimensions[nu];
-      //       temp_site3[mu] = mod(temp_site3[mu] - 1, mu);
-      //       temp_site4[mu] = mod(temp_site4[mu] - 1, mu);
-      //       temp += this->operator()(temp_site1, nu) *
-      //               conj(this->operator()(temp_site2, mu)) *
-      //               conj(this->operator()(temp_site3, mu)) *
-      //               conj(this->operator()(temp_site4, nu)) *
-      //               this->operator()(temp_site4, mu);
-      //       temp_site1 = site;
-      //       temp_site2 = site;
-      //       temp_site3 = site;
-      //       temp_site4 = site;
-      //
-      //       /*
-      //        U(x,μ): ==>
-      //
-      //         ==>
-      //       ^     |
-      //       |     v
-      //
-      //       ^     |
-      //       |     v
-      //         <--
-      // */
-      //       temp_site1[mu] = (temp_site1[mu] + 1) % this->dimensions[mu];
-      //       temp_site1[nu] = mod(temp_site1[nu] - 1, nu);
-      //       temp_site2[nu] = mod(temp_site2[nu] - 2, nu);
-      //       temp_site2[mu] = (temp_site2[mu] + 1) % this->dimensions[mu];
-      //       temp_site3[nu] = mod(temp_site3[nu] - 2, nu);
-      //       temp_site4[nu] = mod(temp_site4[nu] - 1, nu);
-      //       temp += conj(this->operator()(temp_site1, nu)) *
-      //               conj(this->operator()(temp_site2, nu)) *
-      //               conj(this->operator()(temp_site3, mu)) *
-      //               this->operator()(temp_site3, nu) *
-      //               this->operator()(temp_site4, nu);
-      //       temp_site1 = site;
-      //       temp_site2 = site;
-      //       temp_site3 = site;
-      //       temp_site4 = site;
+      auto x_pnu = shift_index_plus(site, nu, 1, dimensions);
+      auto x_mnu = shift_index_minus(site, nu, 1, dimensions);
+
+      auto x_pmu_pnu = shift_index_plus(x_pmu, nu, 1, dimensions);
+      auto x_mmu_pnu = shift_index_plus(x_mmu, nu, 1, dimensions);
+
+      auto x_pmu_mnu = shift_index_minus(x_pmu, nu, 1, dimensions);
+      auto x_mmu_mnu = shift_index_minus(x_mmu, nu, 1, dimensions);
+
+      auto x_p2mu = shift_index_plus(x_pmu, mu, 1, dimensions);
+      auto x_m2mu = shift_index_minus(x_mmu, mu, 1, dimensions);
+
+      auto x_p2nu = shift_index_plus(x_pnu, nu, 1, dimensions);
+      auto x_m2nu = shift_index_minus(x_mnu, nu, 1, dimensions);
+
+      auto x_p2mu_pnu = shift_index_plus(x_p2mu, nu, 1, dimensions);
+      auto x_m2mu_pnu = shift_index_plus(x_m2mu, nu, 1, dimensions);
+      auto x_mmu_p2nu = shift_index_plus(x_mmu_pnu, 1, dimensions);
+
+      auto U1 = conj(field(x_mnu, nu));
+      U1 = U1 * field(x_mnu, mu);
+      U1 = U1 * field(x_mnu_pmu);
+
+      auto U2 = field(x_pmu, nu);
+      auto U3 = field(x_pnu, mu);
+      rect = rect + U1 * U2 * conj(U3);
+
+      U1 = field(site, mu);
+      staple = staple + U1 * U2 * conj(U3);
+      U2 = U1 * U2;
+      U2 = U2 * field(x_pmu_pnu, nu);
+      U2 = U2 * conj(field(x_p2nu, mu));
+      U2 = U2 * conj(field(x_pnu, nu));
+      rect = rect * U2;
+
+      U2 = U1;
+      U2 = U2 * field(x_pmu, mu);
+      U2 = U2 * field(x_p2mu, nu);
+      U2 = U2 * conj(field(x_pmu_pnu, mu));
+      rect = rect + U2 * conj(U3);
+
+      U1 = conj(field(x_mnu, nu));
+      U1 = U1 * conj(field(x_mmu_mnu, mu)) U1 = U1 * field(x_mmu_mnu, nu);
+
+      U2 = field(x_mmu, nu);
+      U3 = field(x_mmu_pnu, mu);
+
+      rect = rect + U1 * U2 * U3;
+      U1 = field(x_mnu, mu);
+      staple = staple + conj(U1) * U2 * U3;
+
+      U2 = conj(U1) * U2;
+      U2 = U2 * field(x_mmu_pnu, nu);
+      U2 = U2 * field(x_mmu_p2nu, mu);
+      U2 = U2 * field(x_mnu, nu);
+
+      rect = rect + U2;
+      U2 = conj(U1);
+      U2 = U2 * conj(field(x_m2mu, mu));
+      U2 = U2 * field(x_m2mu, nu);
+      U2 = U2 * field(x_m2mu_pnu, mu);
+      rect = rect + U2 * U3;
     }
-    return temp;
+    return temp;  // TODO: RETURN TYPE
   }
   void save(std::string filename) const {
     printf("Saving gauge field to file %s\n", filename.c_str());
